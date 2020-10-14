9b583026ec2522aa80f543223c19f4581ff783b1
Boyd_Thurston <boydthurston@gmail.com>
2020-10-15 12:04:29 +1300

    refactor

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,7 +3,6 @@ import Phaser from "phaser"
 class Game extends Phaser.Scene {
 
   init(){
-    this.paddleRightVelocity = new Phaser.Math.Vector2(0,0)
   }
 
   preload(){
@@ -11,53 +10,12 @@ class Game extends Phaser.Scene {
   }
 
   create(){
-    this.ball = this.add.circle(400, 250, 10, 0xffffff, 1)
-    this.physics.add.existing(this.ball) 
 
-    this.ball.body.setCollideWorldBounds(true, 1,1)
-    this.ball.body.setVelocity(Phaser.Math.Between(-200,200), Phaser.Math.Between(-200,200))
-    this.ball.body.setBounce(1, 1)
-
-    this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)
-    this.physics.add.existing(this.paddleLeft, true)
-    this.physics.add.collider(this.paddleLeft, this.ball)
-
-    this.paddleRight = this.add.rectangle(750, 250, 30, 100, 0xffffff, 1)
-    this.physics.add.existing(this.paddleRight, true)
-    this.physics.add.collider(this.paddleRight, this.ball)
-
-    this.cursors = this.input.keyboard.createCursorKeys()
 
   }
 
   update(){
 
-    if(this.cursors.up.isDown){
-      this.paddleLeft.y -= 10
-      this.paddleLeft.body.updateFromGameObject()
-    }
-    else if(this.cursors.down.isDown ){
-      this.paddleLeft.y += 10
-      this.paddleLeft.body.updateFromGameObject()
-    }
-
-    const diff = this.ball.y - this.paddleRight.y
-
-    if(diff < 1){
-      this.paddleRightVelocity.y = -1
-      if (this.paddleRightVelocity.y < -10){
-        this.paddleRightVelocity = -10
-      }
-    }
-    else if(diff > 1){
-      this.paddleRightVelocity.y = 1
-      if (this.paddleRightVelocity.y > 10){
-        this.paddleRightVelocity = 10
-      }
-    }
-
-    this.paddleRight.y += this.paddleRightVelocity.y
-    this.paddleRight.body.updateFromGameObject()
   }
 }
 
