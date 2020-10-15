1fc511cb0b39e2278c5f22dcc63525e195946ae0
Boyd_Thurston <boydthurston@gmail.com>
2020-10-15 15:04:13 +1300

    pseudocode

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -11,6 +11,28 @@ class Game extends Phaser.Scene {
 
   create(){
 
+    // create a menu to be reused
+    // create node to be reused
+    // create link to be reused
+    // create link points to be reused
+
+    // prevent default action from being resolved with right click event
+    this.game.canvas.oncontextmenu = (e) => e.preventDefault()
+    
+    let contextMenuIsDisplaying = false
+    // handle pointer events
+    this.input.on('pointerdown', (pointer) => {
+      if(pointer.rightButtonDown()){
+        // TODO: add validation to know if background/node or link has been right clicked on
+        // TODO: logging to be removed when nolonger needed
+        console.log('context menu pressed')
+        // creates menu box on right click
+        // TODO: seperate menu for reuse once functionality is established
+        this.add.rectangle(pointer.x, pointer.y, 100, 50, 0xffffff, 1)
+        contextMenuIsDisplaying = true
+      }
+      // TODO: destroy menu instance when not in use
+    }, this)
 
   }
 
