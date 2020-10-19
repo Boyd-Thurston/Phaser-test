f122d826bb976cfd3487faed83cc7346b7656ab4
Boyd_Thurston <boydthurston@gmail.com>
2020-10-20 11:46:59 +1300

    refactor

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -18,7 +18,7 @@ class Game extends Phaser.Scene {
 
     // prevent default action from being resolved with right click event
     this.game.canvas.oncontextmenu = (e) => e.preventDefault()
-    
+    let contextMenu
     let contextMenuIsDisplaying = false
     // handle pointer events
     this.input.on('pointerdown', (pointer) => {
@@ -26,9 +26,16 @@ class Game extends Phaser.Scene {
         // TODO: add validation to know if background/node or link has been right clicked on
         // TODO: logging to be removed when nolonger needed
         console.log('context menu pressed')
+        // check to see if menu is already open
+        contextMenuIsDisplaying && contextMenu.destroy()
         // creates menu box on right click
         // TODO: seperate menu for reuse once functionality is established
-        this.add.rectangle(pointer.x, pointer.y, 100, 50, 0xffffff, 1)
+        contextMenu = this.add.container(pointer.x, pointer.y, [
+          this.add.rectangle(0, 0, 100, 20, 0xffffff, 1),
+          this.add.text(-45, -7, 'add wire', {
+            fill: 'black'
+          })
+        ])
         contextMenuIsDisplaying = true
       }
       // TODO: destroy menu instance when not in use
